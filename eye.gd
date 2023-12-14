9ed89889ac959a9b35eb79a4928d679bdc207d2b
jraynolds <jaspirian@gmail.com>
2023-12-14 01:14:48 -0800

    Quick fixes

--- eye.gd
+++ eye.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@onready var raycaster : RayIntersectsPlayer = $Rotateables/RayIntersectsPlayer
-@onready var eyeball : MeshInstance3D = $Rotateables/MeshInstance3D
-@onready var rotate_node : Node3D = $Rotateables
+var raycasters : Array[RayIntersectsPlayer]
+@onready var eyeball : MeshInstance3D = $Armature/Skeleton3D/Eyeball
+@onready var rotate_node : Node3D = $Armature
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 @export var alert_time : float
@@ -14,9 +14,12 @@ var sees_player : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	raycaster.signal_player_found.connect(_player_found)
-	raycaster.signal_player_lost.connect(_player_lost)
-
+	raycasters = []
+	raycasters.append($Armature/RayIntersectsPlayer)
+	raycasters.append($Armature/RayIntersectsPlayer2)
+	raycasters.append($Armature/RayIntersectsPlayer3)
+	raycasters.append($Armature/RayIntersectsPlayer4)
+	raycasters.append($Armature/RayIntersectsPlayer5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -48,12 +51,25 @@ func _play_animation_after_wait():
 	tween.play()
 
 
+func _set_sees_player(sees : bool):
+	if sees:
+		_player_found()
+	else :
+		_player_lost()
+
+
 func _set_alerted(is_alerted : bool):
 	alerted = is_alerted
 	if is_alerted:
 		signal_alerted.emit()
 
 func _physics_process(delta):
+	var currently_sees_player = false
+	for raycaster in raycasters:
+		if raycaster.sees_player:
+			currently_sees_player = true
+	_set_sees_player(currently_sees_player)
+	
 	#pass
 	if sees_player:
 		alert_time_left -= delta
@@ -67,13 +83,17 @@ func _physics_process(delta):
 		)
 		rotate_node.rotation_degrees.z = 0
 		
-	var eyeball_material = eyeball.get_active_material(0) as StandardMaterial3D
 	var weight = 0
 	if sees_player:
 		weight = (alert_time - alert_time_left) / alert_time
 	assert(weight < 1.0)
-	eyeball_material.albedo_color = Color.YELLOW.lerp(Color.RED, weight)
-	eyeball_material.emission = Color.YELLOW.lerp(Color.RED, weight)
+	var override_mat = eyeball.get_active_material(1) as StandardMaterial3D
+	override_mat.albedo_color = Color.YELLOW.lerp(Color.RED, weight)
+	override_mat.emission = Color.YELLOW.lerp(Color.RED, weight)
+	#eyeball.set_surface_override_material(1, )
+	#var eyeball_material = eyeball.get_active_material(0) as StandardMaterial3D
+	#eyeball_material.albedo_color = Color.YELLOW.lerp(Color.RED, weight)
+	#eyeball_material.emission = Color.YELLOW.lerp(Color.RED, weight)
 	#eyeball.mesh.surface_set_material(0, eyeball_material)
 		
 			#Vector3(
